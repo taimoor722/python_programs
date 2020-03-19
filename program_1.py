880e94c21e00d7de94e8fcb1ff511fa41f8212ef
taimoor arif <taimoor722@gmail.com>
2020-03-19 09:19:26 +0500

    more_presentable_print_result_of_means

--- program_1.py
+++ program_1.py
@@ -3,4 +3,4 @@ import numpy as np
 df=pd.DataFrame(data=np.random.randint(1,100,size=(300,5)))
 print(df)
 for i in df.columns:
-    print(df.iloc[:,i].mean())
+    print(round(df.iloc[:,i].mean()),'is mean of column',i)

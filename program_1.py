a1540b1dbc728f6e4b454374b362fc423f2fbadf
taimoor arif <taimoor722@gmail.com>
2020-03-19 09:26:20 +0500

    showing range of columns before Math applied

--- program_1.py
+++ program_1.py
@@ -2,5 +2,6 @@ import pandas as pd
 import numpy as np
 df=pd.DataFrame(data=np.random.randint(1,100,size=(300,5)))
 print(df)
+print(*df.columns.tolist(),'<= columns')
 for i in df.columns:
     print(df.iloc[:,i].mean())
